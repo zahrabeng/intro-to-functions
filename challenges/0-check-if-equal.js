fe1bd872f25abfbfff3eeb83c92c6b359e541390
Zahra <bghzahra@outlook.com>
2022-02-18 11:09:41 +0000

    added testing

--- challenges/0-check-if-equal.js
+++ challenges/0-check-if-equal.js
@@ -5,3 +5,10 @@ function isEqual(numOne, numTwo){
         return false
     }
 }
+
+console.log("isEqual(4, 4)", isEqual(4,4), "should be True");
+console.log("isEqual(-4, -4)", isEqual(-4,-4), "should be True");
+console.log("isEqual(0, 0)", isEqual(0,0), "should be True");
+console.log("isEqual(4, -4)", isEqual(4,-4), "should be False");
+console.log("isEqual(0, 1)", isEqual(0,1), "should be False");
+
